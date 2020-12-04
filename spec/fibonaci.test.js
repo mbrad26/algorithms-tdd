19e0ad4017f9e4e6adcd597965823d492a2d7dea
mbrad26 <mbrad26@gmail.com>
2020-12-04 17:03:08 +0000

    returns the second fibonaci number

--- spec/fibonaci.test.js
+++ spec/fibonaci.test.js
@@ -2,6 +2,10 @@ const { fib } = require('../lib/fibonaci');
 
 describe('fib', () => {
   it('returns the first fibonaci number', () => {
-    expect(fib(1)).toEqual(0);
+    expect(fib(0)).toEqual(0);
+  });
+
+  it('returns the second fibonaci number', () => {
+    expect(fib(1)).toEqual(1);
   });
 });
