0528a09cf56bdf821c1bf13da85c4ddaacf777c4
mbrad26 <mbrad26@gmail.com>
2020-12-04 17:30:42 +0000

    returns the 4th fibonaci number

--- spec/fibonaci.test.js
+++ spec/fibonaci.test.js
@@ -5,6 +5,7 @@ describe('fib', () => {
     [0, 0],
     [1, 1],
     [2, 1],
+    [3, 2],
   ].forEach(item => 
     it(`returns ${item[1]} for fib(${item[0]})`, () => {
       const t0 = performance.now();
