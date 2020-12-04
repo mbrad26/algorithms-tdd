cba6b68a1f5490b5db0b8af9851fc6f692139db4
mbrad26 <mbrad26@gmail.com>
2020-12-04 17:13:46 +0000

    parameterized the test suit

--- spec/fibonaci.test.js
+++ spec/fibonaci.test.js
@@ -1,11 +1,12 @@
 const { fib } = require('../lib/fibonaci');
 
 describe('fib', () => {
-  it('returns the first fibonaci number', () => {
-    expect(fib(0)).toEqual(0);
-  });
-
-  it('returns the second fibonaci number', () => {
-    expect(fib(1)).toEqual(1);
-  });
+  [
+    [0, 0],
+    [1, 1],
+  ].forEach(item => 
+    it(`returns ${item[0]} for fib(${item[1]})`, () => {
+      expect(fib(0)).toEqual(0);
+    })
+  );
 });
