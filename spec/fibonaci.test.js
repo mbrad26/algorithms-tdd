0aec772f4144950e4d638fdda1d823b109944559
mbrad26 <mbrad26@gmail.com>
2020-12-04 17:37:32 +0000

    refactor to final solution

--- spec/fibonaci.test.js
+++ spec/fibonaci.test.js
@@ -8,11 +8,6 @@ describe('fib', () => {
     [3, 2],
   ].forEach(item => 
     it(`returns ${item[1]} for fib(${item[0]})`, () => {
-      const t0 = performance.now();
-      fib(item[0]);
-      const t1 = performance.now();
-      console.log('PERFORMANCE: ', t1 - t0);
-
       expect(fib(item[0])).toEqual(item[1]);
     })
   );
