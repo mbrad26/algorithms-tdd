ad2ea69c5a0799b3aacb43aea0180efefafa4ddf
mbrad26 <mbrad26@gmail.com>
2020-12-20 13:00:04 +0000

    returns [1, 1, 2] for n = 3

--- spec/fibArray.test.js
+++ spec/fibArray.test.js
@@ -8,4 +8,8 @@ describe('generateFirstNArrayOfFibs', () => {
   it('returns [1, 1] for n = 2', () => {
     expect(generateFirstNArrayOfFibs(2)).toEqual([1, 1]);
   });
+
+  it('returns [1, 1, 2] for n = 3', () => {
+    expect(generateFirstNArrayOfFibs(3)).toEqual([1, 1, 2]);
+  });
 });
